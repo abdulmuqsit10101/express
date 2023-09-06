617cad5baed260fbde239957e509c07589b9b561
Nawab Ali <abdulmuqsit10101@gmail.com>
2023-09-06 17:35:07 +0500

    Starting our API: Handling GET requests

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(404).json({ message: 'Hello from the server side !', app: 'Natours' });
-})
+// app.get('/', (req, res) => {
+//     res.status(404).json({ message: 'Hello from the server side !', app: 'Natours' });
+// })
 
-app.post('/', (req, res) => {
-    res.status(200).send('You can post to this end point ... ');
+// app.post('/', (req, res) => {
+//     res.status(200).send('You can post to this end point ... ');
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        tours
+    });
 })
 
 const port = 3000;
