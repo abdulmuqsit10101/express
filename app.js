61e51ca6d2ddfe05965f6cc24ee864f0151f01e2
Nawab Ali <abdulmuqsit10101@gmail.com>
2023-09-17 18:46:18 +0500

    learned and implemented node environment variables

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+
 // * Middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use(express.json())
-app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toDateString();
