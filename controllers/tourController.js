0bf59c9cd815a8bd2a94f86a1e632dca6ec8dfd4
Nawab Ali <abdulmuqsit10101@gmail.com>
2024-02-24 20:58:11 +0500

    feat: worked on the filters and pagination for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,12 +1,57 @@
 const Tour = require('../models/tourModel');
 
+const aliasTop5Tours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 const getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
     const queryObj = { ...req.query };
+
+    // 1A) Filtering
+    // With this we will exclude the following fields from the query to exclude them from the search.
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
-    const query = Tour.find(queryObj);
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 1B) Advance Filtering
+    // With this we can filter using gte and similar expressions
+    // i.e. /api/v1/tours?duration[gte]=5&difficulty=easy&page=2&sort=1&limit=10&price[lt]=1500
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // B) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      // TODO: need to review as its not working, the ratingsAverage is not sorting properly.
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // C) Filed Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // D) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This Page does not exist!');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
@@ -104,4 +149,5 @@ module.exports = {
   getTour,
   updateTour,
   deleteTour,
+  aliasTop5Tours,
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,11 +5,14 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  aliasTop5Tours,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', checkID);
+
+router.route('/top-5-cheap').get(aliasTop5Tours, getAllTours);
 
 router.route('/').get(getAllTours).post(createNewTour);
 
