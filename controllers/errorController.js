08bde752232def75f887a4faa40066f3d647fc25
Nawab Ali <abdulmuqsit10101@gmail.com>
2024-03-14 01:24:45 +0500

    feat: added a duplicate field value

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -37,6 +37,14 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldDB = (err) => {
+  const firstKey = Object.keys(err.keyValue)[0]; // "plainKey"
+  const firstValue = Object.values(err.keyValue)[0];
+  const message = `Duplicate ${firstKey} value: ${firstValue}. Please use another value!`;
+  // 400 is for the bad request!
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -46,6 +54,8 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     if (err.name === 'CastError') {
       err = handleCastErrorDB(err);
+    } else if (err.code === 11000) {
+      err = handleDuplicateFieldDB(err);
     }
 
     sendErrorProd(err, res);
